d9876a87d200d03bf1cdd092afa1134e2383b522
Vinicius Santos <santosviniv@gmail.com>
2023-02-25 17:44:18 -0300

    adicionando svg para o favicon

---
+++ lethiciafachetti/src/pages/WhyTheraphy.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function WhyTheraphy() {
+  return (
+    <div className="bg-cream-light mx-auto py-10 px-40">
+      <h1 className="font-title font-extrabold text-2xl mb-4 text-center whitespace-nowrap">
+        Por que fazer terapia?
+      </h1>
+      <div className="flex flex-row">
+        <div className="bg-cream-medium rounded-2xl p-6 mr-4">
+          <h3 className="font-title font-bold text-xl text-center mb-4">
+            Autoconhecimento
+          </h3>
+          <p>
+            Através do autoconhecimento você entende como lidar melhor com cada
+            uma das suas emoções. Aprende como acolher os sentimentos
+            desconfortáveis e como aproveitar os sentimentos bons. Alcança sua
+            melhor versão através do desenvolvimento pessoal.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WhyTheraphy;
