c34bec3a7ed2a07978e2c1aee19a33fcab207091
Vinicius Santos <santosviniv@gmail.com>
2023-02-28 19:12:39 -0300

    criação dos cards

--- lethiciafachetti/src/pages/WhyTheraphy.jsx
+++ lethiciafachetti/src/pages/WhyTheraphy.jsx
@@ -7,16 +7,75 @@ function WhyTheraphy() {
         Por que fazer terapia?
       </h1>
       <div className="flex flex-row">
-        <div className="bg-cream-medium rounded-2xl p-6 mr-4">
-          <h3 className="font-title font-bold text-xl text-center mb-4">
-            Autoconhecimento
-          </h3>
-          <p>
-            Através do autoconhecimento você entende como lidar melhor com cada
-            uma das suas emoções. Aprende como acolher os sentimentos
-            desconfortáveis e como aproveitar os sentimentos bons. Alcança sua
-            melhor versão através do desenvolvimento pessoal.
-          </p>
+        <div className="bg-cream-medium rounded-2xl p-6 pt-3 mr-4 min-w-[250px] max-h-[320px]">
+          <div className="p-1">
+            <h3 className="font-title font-bold text-xl text-center mb-2 ">
+              Autoconhecimento
+            </h3>
+            <p className="text-[14px] text-justify whitespace-normal">
+              Através do autoconhecimento você entende como lidar melhor com
+              cada uma das suas emoções. Aprende como acolher os sentimentos
+              desconfortáveis e como aproveitar os sentimentos bons. Alcança sua
+              melhor versão através do desenvolvimento pessoal.
+            </p>
+          </div>
+        </div>
+        <div className="bg-cream-medium rounded-2xl p-6 pt-3 mr-4 min-w-[250px] max-h-[320px]">
+          <div className="p-1">
+            <h3 className="font-title font-bold text-xl text-center mb-2">
+              Relacionamentos
+            </h3>
+            <p className="text-[14px] text-justify">
+              Se conhecendo, você aprende sobre seus limites e consegue se impor
+              melhor nas suas relações, além de se cuidar com mais cuidado para
+              não ultrapassar nenhum desses limites. Aprende a comunicar o que
+              te incomoda e entende que não precisa mudar nada sobre você para
+              se encaixar em relações desrespeitosas.
+            </p>
+          </div>
+        </div>
+        <div className="bg-cream-medium rounded-2xl p-4 pt-1 mr-4 min-w-[250px] max-h-[320px]">
+          <div className="p-1 h-[231px]">
+            <h3 className="font-title font-bold text-xl text-center mb-2">
+              Autoestima e Autossabotagem
+            </h3>
+            <p className="text-[14px] text-justify ">
+              A autoestima está diretamente relacionada ao ato de se conhecer.
+              Autoestima vai além de aceitação do físico. É você conhecer suas
+              qualidades e virtudes, entender que você mesma pode se cuidar, que
+              você mesma pode se mimar em um dia ruim e deixar o medo de ficar
+              sozinha de lado.
+            </p>
+          </div>
+        </div>
+        <div className="bg-cream-medium rounded-2xl p-6 pt-3 mr-4 min-w-[250px] max-h-[320px]">
+          <div className="p-1">
+            <h3 className="font-title font-bold text-xl text-center mb-2">
+              Escuta e Ajuda
+            </h3>
+            <p className="text-[14px] text-justify">
+              Se precisar se olhar com cuidado e atenção para lidar com
+              situações como ansiedade, depressão, hábitos prejudiciais,
+              compulsões ou manias, medos ou fobias, dificuldades, traumas,
+              estresse, dependência emocional, transtornos ou se sentir que
+              precisa de ajuda, ainda que não identifique com clareza o motivo.
+            </p>
+          </div>
+        </div>
+        <div className="bg-cream-medium rounded-2xl p-6 pt-3 mr-4 min-w-[250px] max-h-[320px]">
+          <div className="p-1">
+            <h3 className="font-title font-bold text-xl text-center mb-2">
+              Perdas e Luto
+            </h3>
+            <p className="text-[14px] text-justify">
+              Engana-se quem pensa que luto é apenas sobre perder uma alguém.
+              Enfrentamos momentos de luto nos encerramentos de ciclo.
+              Separações, perdas, fases de transições e mudanças podem nos levar
+              a enfrentar períodos de luto, é importante saber acolher esse
+              sentimento, entender o que significa o encerramento de ciclo para
+              você.
+            </p>
+          </div>
         </div>
       </div>
     </div>
