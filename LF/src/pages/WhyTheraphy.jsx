07a998283e3d20160df7b12e042f2c18b418e568
Vinicius Santos <santosviniv@gmail.com>
2023-03-07 02:00:03 -0300

    fix: correção na responsividade do slider

--- LF/src/pages/WhyTheraphy.jsx
+++ LF/src/pages/WhyTheraphy.jsx
@@ -30,19 +30,22 @@ function WhyTheraphy() {
     pagination: { clickable: true },
     breakpoints: {
       // when window width is >= 320px
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 5,
+      530: {
+        slidesPerView: 1,
+        // eslint-disable-next-line prettier/prettier
+        spaceBetween: 5
       },
       // when window width is >= 480px
-      480: {
+      960: {
         slidesPerView: 3,
-        spaceBetween: 30,
+        // eslint-disable-next-line prettier/prettier
+        spaceBetween: 30
       },
       // when window width is >= 640px
-      640: {
+      1300: {
         slidesPerView: 4,
-        spaceBetween: 40,
+        // eslint-disable-next-line prettier/prettier
+        spaceBetween: 40
       },
     },
   };
