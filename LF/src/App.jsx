7af6dc405a2a30d68f3269c6cb7225ce36078770
Vinicius Santos <santosviniv@gmail.com>
2023-03-07 05:06:07 -0300

    feat: benefits page

--- LF/src/App.jsx
+++ LF/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import About from './pages/About/About';
+import Benefits from './pages/Benefits/Benefits';
 import Main from './pages/Main/Main';
 import WhyTheraphy from './pages/WhyTheraphy/WhyTheraphy';
 import GlobalStyle from './styles/globalStyles';
@@ -13,6 +14,7 @@ function App() {
       <Main />
       <WhyTheraphy />
       <About />
+      <Benefits />
     </>
   );
 }
---
+++ LF/src/pages/Benefits/Benefits.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledBenefits = styled.div`
+  background-color: #FFF5E9;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 20px auto;
+  padding: 40px;
+
+  & h2 {
+    text-align: center;
+    font-family: Playfair Display, serif;
+    font-weight: 800;
+    margin-top: 0;
+  }
+
+  & div {
+    display: flex;
+    justify-content: space-between;
+  }
+  @media (min-width: 530px) and (max-width: 960px) {
+    & div {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+
+`;
+
+const StyledCard = styled.div`
+  background-color:#8ACFB2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border-radius: 1rem;
+  padding: 1rem 1rem 3rem 1rem;
+  margin: 1rem 1rem 1rem 1rem;
+  width: 300px;
+  height: 200px;
+
+  & h3 {
+    text-align:center;
+    margin-bottom: 0;
+  }
+
+  & p { 
+    text-align: justify;
+    
+  }
+  
+`;
+
+function Benefits() {
+  return (
+    <StyledBenefits>
+      <h2>Principais Benefícios da Terapia</h2>
+      <div className="benefits-content">
+        <StyledCard>
+          <h3>Praticidade</h3>
+          <p>
+            Com a psicoterapia online, você consegue encaixar com maior
+            facilidade o autocuidado na sua rotina, fazendo sua consulta em casa
+            ou no trabalho. Não precisa se preocupar com locomoção, trânsito e
+            tempo gasto no trajeto.
+          </p>
+        </StyledCard>
+        <StyledCard>
+          <h3>Autocuidado</h3>
+          <p>
+            Com a psicoterapia você consegue estabelecer e comunicar seus
+            limites, melhorando assim o seu relacionamento com os outros e com
+            você mesma. Entende que dias ruins também passam, que acolher seus
+            sentimentos é importante e que dizer “não” é necessário.
+          </p>
+        </StyledCard>
+      </div>
+    </StyledBenefits>
+  );
+}
+
+export default Benefits;
