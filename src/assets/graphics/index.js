215227bf4ef2fa721e3b08278b6a7ae3b8332c1d
Vinicius Santos <santosviniv@gmail.com>
2023-03-09 20:24:27 -0300

    🎨: adição dos grafismos

---
+++ src/assets/graphics/index.js
@@ -0,0 +1,11 @@
+import el1 from './01.svg';
+import el2 from './02.svg';
+import el3 from './03.svg';
+import el4 from './04.svg';
+import el5 from './05.svg';
+import el6 from './06.svg';
+import el7 from './07.svg';
+import el8 from './08.svg';
+
+export { el1, el2, el3, el4, el5, el6, el7, el8 };
+
